582496fda4733b9a94ce272c113de1427e9e20a7
Kanishk <kanishk@softobiz.com>
2022-01-17 09:36:32 +0530

    initial commit

--- crud-app/src/Components/Add.tsx
+++ crud-app/src/Components/Add.tsx
@@ -3,8 +3,15 @@ import { Form, Button } from "react-bootstrap";
 // import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Add = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   let history = useHistory();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -19,49 +26,79 @@ const Add = () => {
   return (
     <React.Fragment>
       <h1 className="text-center mt-3">User Add Screen</h1>
-      <Form className="d-grid gap-2 text-center">
+      <Form
+        onSubmit={handleSubmit((event) => postData())}
+        className="d-grid gap-2 text-center"
+      >
         <Form.Group controlId="name">
           <Form.Label className="">Name</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Name"
+            // required
+            {...register("name", {
+              required: "Name is required...!",
+              pattern: {
+                value: /^[A-Za-z]/,
+                message: "Invalid Name...!",
+              },
+              minLength: 3,
+              maxLength: 30,
+            })}
+            // minLength=3
+            // maxLength=30
             value={name}
             onChange={(event) => setName(event.target.value)}
           />
+          {errors.name && errors.name.message}
         </Form.Group>
         <Form.Group controlId="email">
           <Form.Label className="">Email</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Email"
+            // required
+            {...register("email", {
+              required: "E_mail is required...!",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid E_mail...!",
+              },
+            })}
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
+          {errors.email && errors.email.message}
         </Form.Group>
         <Form.Group controlId="age">
           <Form.Label className="">Age</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Age"
+            required
+            {...register("age", {
+              required: "Date of Birth is required...!",
+            })}
             value={age}
             onChange={(event) => setAge(event.target.value)}
           />
+          {errors.age && errors.age.message}
         </Form.Group>
         <Form.Group controlId="gender">
           <Form.Label className="">Gender</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Gender"
+            required
+            {...register("gender", {
+              required: "Date of Birth is required...!",
+            })}
             value={gender}
             onChange={(event) => setGender(event.target.value)}
           />
+          {errors.gender && errors.gender.message}
         </Form.Group>
-        <Button
-          type="submit"
-          className="mt-3 primary"
-          variant="primary"
-          onClick={postData}
-        >
+        <Button type="submit" className="mt-3 primary" variant="primary">
           Add User
         </Button>
       </Form>
--- crud-app/src/Components/Edit.tsx
+++ crud-app/src/Components/Edit.tsx
@@ -3,11 +3,18 @@ import { Form, Button } from "react-bootstrap";
 // import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { useHistory, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Edit = () => {
   let history = useHistory();
   let params: any = useParams();
   const id = params.id;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -41,48 +48,80 @@ const Edit = () => {
   return (
     <React.Fragment>
       <h1 className="text-center mt-3">User Edit Screen</h1>
-      <Form className="d-grid gap-2 text-center">
+      <Form
+        onSubmit={handleSubmit((event) => updateData())}
+        className="d-grid gap-2 text-center"
+      >
         <Form.Group controlId="name">
           <Form.Label className="">Name</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Name"
+            // required
+            {...register("name", {
+              required: "Name is required...!",
+              pattern: {
+                value: /^[A-Za-z]/,
+                message: "Invalid Name...!",
+              },
+              minLength: 3,
+              maxLength: 30,
+            })}
             value={name}
             onChange={(event) => setName(event.target.value)}
           />
+          {errors.name && errors.name.message}
         </Form.Group>
         <Form.Group controlId="email">
           <Form.Label className="">Email</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Email"
+            // required
+            {...register("email", {
+              required: "E_mail is required...!",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid E_mail...!",
+              },
+            })}
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
+          {errors.email && errors.email.message}
         </Form.Group>
         <Form.Group controlId="age">
           <Form.Label className="">Age</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Age"
+            required
+            {...register("age", {
+              required: "Date of Birth is required...!",
+            })}
             value={age}
             onChange={(event) => setAge(event.target.value)}
           />
+          {errors.age && errors.age.message}
         </Form.Group>
         <Form.Group controlId="gender">
           <Form.Label className="">Gender</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter Gender"
+            required
+            {...register("gender", {
+              required: "Date of Birth is required...!",
+            })}
             value={gender}
             onChange={(event) => setGender(event.target.value)}
           />
+          {errors.gender && errors.gender.message}
         </Form.Group>
         <Button
           type="submit"
           className="mt-3"
           variant="primary"
-          onClick={updateData}
         >
           Update User
         </Button>
